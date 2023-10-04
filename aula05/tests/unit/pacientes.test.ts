e4a1d32eb34ce9bd2c1d14b956d0a715b5c79761
nbalica <nbalica@alelo.com.br>
2023-10-03 21:45:22 -0300

    aula 05 feita

--- aula05/tests/unit/pacientes.test.ts
+++ aula05/tests/unit/pacientes.test.ts
@@ -1,5 +1,24 @@
+import { generateProtocolForPacient } from "protocols-generator";
+import { v4 as uuidv4 } from 'uuid';
+
+jest.mock("uuid", () => {
+  return {
+    v4: () => { return "valor simulado no mock" }
+  }
+})
+
 describe("calculator tests", () => {
   it("should work", async () => {
     expect(true).toBe(true);
   });
+
+  it("should generate the protocols", () => {
+    const protocols = generateProtocolForPacient('Lucas', 'Silva', true)
+    expect(protocols).toEqual({
+      priority: true,
+      date: expect.any(Date),
+      pacient: 'Lucas Silva',
+      protocol: "valor simulado no mock", 
+    })
+  })
 });
